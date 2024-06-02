e41c6643692e83c1ad33ab9896eeac997b193f0e
Allan Juma <allancjuma@gmail.com>
2024-06-02 04:12:01 +0300

    home page styling

--- src/components/home/HomePage.jsx
+++ src/components/home/HomePage.jsx
@@ -1,7 +1,15 @@
 import React, { useEffect, useState } from "react";
 import { getUsers, getAllAlbums } from "../../services/index";
 import { Link } from "react-router-dom";
-import { Grid, Typography } from "@mui/material";
+import {
+  Avatar,
+  Box,
+  Card,
+  CardContent,
+  CardHeader,
+  Grid,
+  Typography,
+} from "@mui/material";
 
 const HomePage = () => {
   const [users, setUsers] = useState([]);
@@ -21,15 +29,65 @@ const HomePage = () => {
     fetchAlbums();
   }, []);
 
+  const getAlbumsByUserId = (userId) => {
+    const userAlbums = albums.filter((album) => album.userId === userId);
+    return userAlbums.length;
+  };
+
   return (
-    <Grid container>
-      <Typography variant="h2">User List</Typography>
-      {users.map((user) => (
-        <li key={user.id}>
-          <Link to={`/user/${user.id}`}>{user.name}</Link>
-        </li>
-      ))}
-    </Grid>
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        gap: 2,
+      }}
+    >
+      <Typography variant="h5">User List</Typography>
+      <Grid
+        container
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          gap: 2,
+          flexWrap: "wrap",
+          flexFlow: "row wrap",
+          // maxWidth: 700,
+        }}
+      >
+        {users.map((user) => (
+          <Grid
+            item
+            sx={{
+              minWidth: 300,
+            }}
+          >
+            <Card key={user.id}>
+              <CardHeader
+                sx={{
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  gap: 10,
+                }}
+                avatar={
+                  <Avatar sx={{ width: 60, height: 60 }}>
+                    {user.username[0]}
+                  </Avatar>
+                }
+                title={user.username}
+                subheader={`Albums: ${getAlbumsByUserId(user.id)}`}
+              />
+              <CardContent>
+                <Link to={`/user/${user.id}`}>View User</Link>
+              </CardContent>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
+    </Box>
   );
 };
 
