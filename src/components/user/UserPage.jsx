6d92429fcb734f044aacb11bbbcdebdb072b67b1
Allan Juma <allancjuma@gmail.com>
2024-06-02 04:23:52 +0300

    add: userpage, data fetching

--- src/components/user/UserPage.jsx
+++ src/components/user/UserPage.jsx
@@ -1,7 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams, Link } from "react-router-dom";
+import { getUser, getUserAlbums } from "../../services";
 
 const UserPage = () => {
-  return <div>UserPage</div>;
+  const { userId } = useParams();
+  const [albums, setAlbums] = useState([]);
+  const [user, setUser] = useState([]);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      const data = await getUser(userId);
+      setUser(data);
+    };
+
+    const fetchAlbums = async () => {
+      const data = await getUserAlbums(userId);
+      setAlbums(data);
+    };
+    fetchUser();
+    fetchAlbums();
+  }, [userId]);
+
+  console.log(userId);
+
+  return (
+    <div>
+      <h1>User Albums</h1>
+      <p>Total Albums: {albums.length}</p>
+      <ul>
+        {albums.map((album) => (
+          <li key={album.id}>
+            <Link to={`/album/${album.id}`}>{album.title}</Link>
+          </li>
+        ))}
+      </ul>
+      <Link to="/home">Back to Home</Link>
+    </div>
+  );
 };
 
 export default UserPage;
