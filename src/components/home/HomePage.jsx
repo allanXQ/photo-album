ba771f6c60a846629d5de1890e383db9226ba501
Allan Juma <allancjuma@gmail.com>
2024-06-02 03:17:20 +0300

    add: homepage- fetch users and albums

--- src/components/home/HomePage.jsx
+++ src/components/home/HomePage.jsx
@@ -1,7 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { getUsers, getAllAlbums } from "../../services/index";
+import { Link } from "react-router-dom";
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  const [users, setUsers] = useState([]);
+  const [albums, setAlbums] = useState([]);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const data = await getUsers();
+      setUsers(data);
+    };
+    const fetchAlbums = async () => {
+      const data = await getAllAlbums();
+      setAlbums(data);
+    };
+
+    fetchUsers();
+    fetchAlbums();
+  }, []);
+
+  return (
+    <div>
+      <h1>User List</h1>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>
+            <Link to={`/user/${user.id}`}>{user.name}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default HomePage;
