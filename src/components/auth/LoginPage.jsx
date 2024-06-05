b0775a60bef640a362812220d5b46f1598f474b1
Allan Juma <allancjuma@gmail.com>
2024-06-05 03:08:14 +0300

    update: page content

--- src/components/auth/LoginPage.jsx
+++ src/components/auth/LoginPage.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import { useAuth } from "../../context/AuthContext";
 import { GoogleLogin } from "@react-oauth/google";
-import { jwtDecode } from "jwt-decode";
+import { useAuth } from "../../context/AuthContext"; // Ensure this context provides the login method
+import { Box, Typography, Container } from "@mui/material";
 
 const LoginPage = () => {
   const navigate = useNavigate();
   const { login } = useAuth();
 
   const handleLoginSuccess = (credentialResponse) => {
-    const decodedToken = jwtDecode(credentialResponse.credential);
-    console.log("Login Success:", decodedToken);
+    console.log("Login Success:", credentialResponse);
+    const decodedToken = jwtDecode(credentialResponse.credential); // Assuming jwtDecode to decode the Google token
     login({
       name: decodedToken.name,
       email: decodedToken.email,
@@ -20,19 +20,28 @@ const LoginPage = () => {
   };
 
   return (
-    <div
-      style={{
+    <Container
+      component="main"
+      maxWidth="xs"
+      sx={{
+        mt: 8,
+        mb: 4,
         display: "flex",
-        justifyContent: "center",
+        flexDirection: "column",
         alignItems: "center",
-        height: "100vh",
       }}
     >
-      <GoogleLogin
-        onSuccess={handleLoginSuccess}
-        onError={() => console.log("Login Failed")}
-      />
-    </div>
+      <Typography component="h1" variant="h5">
+        Sign In
+      </Typography>
+      <Box sx={{ mt: 1 }}>
+        <GoogleLogin
+          onSuccess={handleLoginSuccess}
+          onError={() => console.log("Login Failed")}
+          // theme="filled_black" // A theme option provided by the GoogleLogin component to style the button
+        />
+      </Box>
+    </Container>
   );
 };
 
--- src/components/landing/LandingPage.jsx
+++ src/components/landing/LandingPage.jsx
@@ -1,45 +1,32 @@
 import React from "react";
+import { Container, Typography, Box, Button } from "@mui/material";
 import { useNavigate } from "react-router-dom";
-import { GoogleLogin } from "@react-oauth/google";
-import { useAuth } from "../../context/AuthContext"; // Ensure this context provides the login method
-import { Box, Typography, Button, Container } from "@mui/material";
 
 const LandingPage = () => {
   const navigate = useNavigate();
-  const { login } = useAuth();
-
-  const handleLoginSuccess = (credentialResponse) => {
-    console.log("Login Success:", credentialResponse);
-    // Assuming jwtDecode to decode the Google token
-    const decodedToken = jwtDecode(credentialResponse.credential);
-    login({
-      name: decodedToken.name,
-      email: decodedToken.email,
-      tokenExpiry: decodedToken.exp,
-    });
-    navigate("/home");
-  };
 
   return (
-    <Container component="main" maxWidth="sm" sx={{ mt: 8, mb: 4 }}>
+    <Container component="main" maxWidth="md" sx={{ mt: 8, mb: 4 }}>
       <Box
         sx={{
           marginTop: 8,
           display: "flex",
           flexDirection: "column",
           alignItems: "center",
+          gap: 2,
         }}
       >
-        <Typography component="h1" variant="h4">
+        <Typography component="h1" variant="h4" gutterBottom>
           Welcome to Album Viewer!
         </Typography>
-        <Typography variant="h6" sx={{ mt: 2, mb: 2, textAlign: "center" }}>
-          Explore and manage your photo collections seamlessly.
+        <Typography variant="h6" sx={{ textAlign: "center" }}>
+          Explore and manage your photo collections with ease. Album Viewer
+          allows you to organize, view, and edit your albums efficiently,
+          bringing all your memories into one place.
         </Typography>
-        <GoogleLogin
-          onSuccess={handleLoginSuccess}
-          onError={() => console.log("Login Failed")}
-        />
+        <Button variant="contained" onClick={() => navigate("/login")}>
+          Get Started
+        </Button>
       </Box>
     </Container>
   );
