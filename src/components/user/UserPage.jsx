0d8210fc1ea48a431568439ce2576bddc2be05ac
Allan Juma <allancjuma@gmail.com>
2024-06-06 12:06:01 +0300

    fix: react not defined,

--- src/components/user/UserPage.jsx
+++ src/components/user/UserPage.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import { getUser, getUserAlbums, getAlbumPhotos } from "../../services";
 import {
@@ -50,7 +50,7 @@ const UserPage = () => {
   return (
     <Box sx={{ padding: 4 }}>
       <Typography variant="h4" gutterBottom>
-        {user?.username}&quot;s Albums
+        {user?.username}&apos;s Albums
       </Typography>
       <Typography variant="subtitle1" gutterBottom>
         Total Albums: {albums.length}
